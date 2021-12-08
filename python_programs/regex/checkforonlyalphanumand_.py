a2599822f7f785f4c3ad644c33b5716cb277a302
Lioncat2002 <sankhayan2002@gmail.com>
2021-12-08 11:56:17 +0530

    updates

--- python_programs/regex/checkforonlyalphanumand_.py
+++ python_programs/regex/checkforonlyalphanumand_.py
@@ -4,7 +4,7 @@ WAP to check if a string has only a-z A-Z 0-9 and _
 import re
 n=input()
 
-if re.match("^[a-zA-Z0-9_]*$",n):
+if re.match("^[a-zA-Z0-9_\s]*$",n):
     print("Valid")
 else:
     print("Invalid")
