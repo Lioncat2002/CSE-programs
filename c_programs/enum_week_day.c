1b477303cf6a62d9f775902f58b9c118cd1ec0a7
Lioncat2002 <sankhayan2002@gmail.com>
2022-02-03 18:31:34 +0530

    adding c code

---
+++ c_programs/enum_week_day.c
@@ -0,0 +1,19 @@
+#include "stdio.h"
+
+enum week{
+Sunday,
+Monday,
+Tuesday,
+Wednesday,
+Thursday,
+Friday,
+Saturday,
+};
+
+int main(){
+
+    enum week w;
+    w=Monday;
+    printf("%d",Sunday);
+
+}
