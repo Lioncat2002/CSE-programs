8e6f823dad8b927f5ab361626bc8b067705c169f
lioncat2002 <sankhayan2002@gmail.com>
2022-03-26 12:23:08 +0530

    more programs
    Signed-off-by: lioncat2002 <sankhayan2002@gmail.com>

--- c_programs/misc/test.c
+++ c_programs/misc/test.c
@@ -1,16 +1,11 @@
 #include "stdio.h"
+
 int main()
 {
-int i, j;
-char a[2] [3] = { {'a', 'b', 'c'}, {'d', 'e', 'f'}};
-char b[3] [2];
-char *p = *b;
-for (i=0; i<2; i++)
-{
- for(j=0; j<3; j++)
- {
- *(p+2 * j + i) = a[i][j];
- }
- }
- printf("%s",b);
+    int i;
+int a[]={1,2,3,4};
+while(i<20){
+printf("%d\n",a[i]);
+i++;
+}
 }
