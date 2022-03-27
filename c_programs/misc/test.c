4691b7683a01baa7534c5ec8e80fee7f1cdbabc6
lioncat2002 <sankhayan2002@gmail.com>
2022-03-27 13:07:29 +0530

    more programs
    Signed-off-by: lioncat2002 <sankhayan2002@gmail.com>

--- c_programs/misc/test.c
+++ c_programs/misc/test.c
@@ -1,11 +1,20 @@
 #include "stdio.h"
 
-int main()
-{
-    int i;
-int a[]={1,2,3,4};
-while(i<20){
-printf("%d\n",a[i]);
-i++;
-}
+int main(){
+    char s[200];
+    printf("Enter a string: ");
+    fgets(s,200,stdin);
+    printf(s);
+    char *p=s;
+    for (int i = 0; *(p+i) !='\0'; i++)
+    {
+        
+        if (*(p+i)=='A'||*(p+i)=='E'||*(p+i)=='I'||*(p+i)=='O'||*(p+i)=='U')
+        {
+            *(p+i)=*(p+i)+1;
+            
+        }
+    }
+    printf(s);
+    
 }
